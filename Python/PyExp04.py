2785f7b7967ef919806a5f9e9bc8d10f766e9dfc
Shubh Shah <80100899+DarkSoul14789@users.noreply.github.com>
2022-04-05 00:57:52 +0530

    Update PyExp04.py

--- Python/PyExp04.py
+++ Python/PyExp04.py
@@ -1,10 +1,3 @@
-'''
-
-                            Online Python Debugger.
-                Code, Run and Debug Python program online.
-Write your code in this editor and press "Debug" button to debug program.
-
-'''
 
 #Q1) Create a Triangle from * sign of length 10 --> INCORRECT
 n=40
